b85f84dcf21b544fc7fd645c3294bb0a89e6f036
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-05 15:48:45 +0530

    random_quotes_api.py functionality done

--- random_quotes_api.py
+++ random_quotes_api.py
@@ -22,40 +22,57 @@ PLACEHOLDER = "Hello !"
 window = tk.Tk()
 window.title("Random Quotes")
 window.minsize(400, 400)
-window.config(padx=20, pady=20, bg = "PaleTurquoise")
+window.config(padx=20, pady=20, bg = COLOR_BG )
+
+
 label1 = tk.Label(text="Random Quotes", font= H_FONT)
-label1.config(bg = "PaleTurquoise", highlightthickness = 0, padx = 20, pady = 30)
-label1.grid(column=0, row=0)
+label1.config(bg = COLOR_BG, fg= COLOR_TEXT, highlightthickness = 0, padx = 20, pady = 30, justify = "center")
+label1.grid(column=0, row=0,columnspan=2)
 
 # TODO 2: Add a large Label widget to display the quote text
 
-quote_label = tk.Label(text=PLACEHOLDER, font= M_FONT)
-quote_label.config(bg = "PaleTurquoise", highlightthickness = 0, padx = 10, pady = 10)
-quote_label.grid(column=1, row=0)
-quote_label.grid(column=0, row=1)
+quote_label = tk.Label(text="Quote goes here", font= M_FONT)
+quote_label.config(bg=COLOR_BG, fg=COLOR_TEXT, wraplength=350, justify="center", padx=10, pady=10)
+quote_label.grid(column=0, row=1, columnspan=2)
 # TODO 3: Add a smaller Label widget below to display the author name
 
 author_label = tk.Label(text=PLACEHOLDER, font= S_FONT)
 author_label.config(bg = "PaleTurquoise", highlightthickness = 0, padx = 10, pady = 10)
-author_label.grid(column=0, row=2)
+author_label.grid(column=1, row=2)
 
 # TODO 4: Add a Button widget labeled "New Quote"
-new_quote_btn = tk.Button(text="New Quote", font= S_FONT)
-new_quote_btn.config(bg = "PaleTurquoise", highlightthickness = 0, padx = 10, pady = 10)
-new_quote_btn.grid(column=0, row=3)
+new_quote_btn = tk.Button(text="New Random Quote", font= S_FONT)
+new_quote_btn.config(
+    bg=COLOR_PRIMARY,
+    fg=COLOR_BUTTON_TEXT,
+    activebackground=COLOR_HIGHLIGHT,
+    activeforeground=COLOR_BUTTON_TEXT,
+    relief="groove",
+    bd=2,
+    padx=10,
+    pady=10,
+)
+#     relief="raised"
+# new_quote_btn.config(bg = "PaleTurquoise", highlightthickness = 0, padx = 10, pady = 10)
+new_quote_btn.grid(column=0, row=3, columnspan=2)
 # TODO 5: Write a function that makes a GET request to https://zenquotes.io/api/random
 
 def random_quote():
     try:
+        new_quote_btn.config(text= "loading", state="disabled")
+        window.update()
         response = requests.get(api_url)
         quote_api_response = response.json()
         new_quote = quote_api_response[0]["q"]
         new_author = quote_api_response[0]["a"]
         quote_label.config(text=new_quote)
-        author_label.config(text=new_author)
+        author_label.config(text=f"- {new_author}")
+        new_quote_btn.config(text = "New Random Quote",state="normal")
 
     except  requests.exceptions.RequestException as e:
-        print(e)
+        quote_label.config("Failed to fetch quote. Please try again.")
+        author_label.config(e)
+        window.update()
         return e
 
 # TODO 6: Parse the JSON response to extract the quote ("q") and author ("a")
